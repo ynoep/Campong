c2ed544f3c68e725b548d2880ad984624bc35501
bibi <ab9647@gmail.com>
2023-02-08 14:41:33 +0900

    게시판 추가2

---
+++ CAMPONG/src/main/java/com/multi/campong/board/model/vo/Board.java
@@ -0,0 +1,25 @@
+package com.multi.campong.board.model.vo;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+	private int bNo;
+	private int uNo;
+	private String title;
+	private String content;
+	private String originalFileName;
+	private String renamedFileName;
+	private String status;
+	private Date createDate;
+	private int readCount;
+	private String type;
+	private List<Reply> replyList;
+}
